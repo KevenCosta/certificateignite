a566243332f62953953d5033c8a026a37119854f
KevenCosta <kevencosta129@gmail.com>
2022-01-26 23:07:19 -0300

    M7 Pos Deploy AWS

---
+++ src/utils/dynamodbClient.ts
@@ -0,0 +1,20 @@
+import { DynamoDB } from "aws-sdk"
+
+
+const options = {
+    region: "localhost",
+    endpoint: "http://localhost:8000",
+    accessKeyId: "x",
+    secretAccessKey: "x"
+}
+
+const isOffline = ()=>{
+    //variavel do plugin serverless offline, ele atribui valor
+    return process.env.IS_OFFLINE; 
+}
+
+export const document = isOffline()
+? new DynamoDB.DocumentClient(options)
+: new DynamoDB.DocumentClient();
+
+//usa local se offline ou da aws se online
